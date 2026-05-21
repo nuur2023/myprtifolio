f087736423f99ee2044a99f7bd653422eecbcdc4
nuur2023 <nuuradiinhassan45@gmail.com>
2026-05-21 14:37:28 +0300

    new updated in 5-21-2026

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,22 +2,23 @@ import mobile_login from "../../public/images/mobile_login.jpg";
 import taskImage from "../../public/images/task_image.jpg"; 
 import abaalbook from "../../public/images/abaalbook.jpg"; 
 import gasImg from "../../public/images/gas.jpg";
+import gas_app from "../../public/images/gas_app.jpg";
 
 export const projects = [
+  // {
+  //   id: 1,
+  //   name: "Doogsan Accounting System",
+  //   description: "A full-stack accounting system for Doogsan, features include inventory management, sales tracking, customer management, and admin dashboard.",
+  //   image: "https://images.unsplash.com/photo-1556742049-0cfed4f6a45d?w=800",
+  //   type: "web",
+  //   techStack: ["React js","Tailwind CSS", ".Net Core 8", "MySQL"],
+  //   liveUrl: "https://www.doogsan.abaalsoftware.com/login",
+  //   githubUrl: "",
+  //   playStoreUrl: null,
+  //   featured: true
+  // },
   {
     id: 1,
-    name: "Doogsan Accounting System",
-    description: "A full-stack accounting system for Doogsan, features include inventory management, sales tracking, customer management, and admin dashboard.",
-    image: "https://images.unsplash.com/photo-1556742049-0cfed4f6a45d?w=800",
-    type: "web",
-    techStack: ["React js","Tailwind CSS", ".Net Core 8", "MySQL"],
-    liveUrl: "https://www.doogsan.abaalsoftware.com/login",
-    githubUrl: "",
-    playStoreUrl: null,
-    featured: true
-  },
-  {
-    id: 2,
     name: "Task Management Web App",
     description: "A web application for task and project management. Built with React JS, .Net Core, MySQL, and Bootstrap.",
     image: taskImage,
@@ -29,7 +30,7 @@ export const projects = [
     featured: true
   },
   {
-    id: 4,
+    id: 2,
     name: "Water Supply Mobile App",
     description: "A water supply mobile application connecting customers with water suppliers. Features include real-time order tracking, multiple payment options, water supplier ratings, and push notifications.",
     image: mobile_login,
@@ -41,7 +42,7 @@ export const projects = [
     featured: false
   },
   {
-    id: 5,
+    id: 3,
     name: "Loan and Purchase Invoices Management App",
     description: "A mobile application for managing loan and purchase invoices. Features include invoice creation, payment tracking, due date reminders, and financial reporting.",
     image: abaalbook,
@@ -53,7 +54,7 @@ export const projects = [
     featured: false
   },
   {
-    id: 6,
+    id: 4,
     name: "Gas Station Management System",
     description: "A full-stack accounting system for Gas Stations, features include inventory management, sales tracking, customer management, Employees Management, Pay Rolls and Admin dashboard Plus Multi station support and many more features.",
     image: gasImg,
@@ -64,6 +65,19 @@ export const projects = [
     playStoreUrl: null,
     featured: true
   },
+  // Mobile gas station // https://play.google.com/apps/testing/com.abaalsoftware.gas_station
+  {
+    id: 5,
+    name: "Gas Station Mobile App",
+    description: "A mobile application for gas station management, features include inventory management, sales tracking, customer management, Dipping balance tracking, and Station Overview dashboard Plus Multi station support and many more features.",
+    image: gas_app,
+    type: "mobile",
+    techStack: ["Flutter", "GetX", "Dart"],
+    liveUrl: "https://play.google.com/store/apps/details?id=com.abaalsoftware.gas_station",
+    githubUrl: null,
+    playStoreUrl: "https://play.google.com/apps/testing/com.abaalsoftware.gas_station",
+    featured: false
+  }
 ]
 
 
