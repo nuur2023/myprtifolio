440c62ee2d640d5e453709882cf6b09414348cdb
nuur2023 <nuuradiinhassan45@gmail.com>
2026-06-07 07:07:45 +0300

    new update by removed gas station and its mobile app in 6-7-2026

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,8 +1,8 @@
 import mobile_login from "/images/mobile_login.jpg";
 import taskImage from "/images/task_image.jpg"; 
 import abaalbook from "/images/abaalbook.jpg"; 
-import gasImg from "/images/gas.jpg";
-import gas_app from "/images/gas_app.jpg";
+// import gasImg from "/images/gas.jpg";
+// import gas_app from "/images/gas_app.jpg";
 
 export const projects = [
   // {
@@ -53,31 +53,31 @@ export const projects = [
     playStoreUrl: "https://play.google.com/store/apps/details?id=com.abaalsoftware.abaalbook",
     featured: false
   },
-  {
-    id: 4,
-    name: "Gas Station Management System",
-    description: "A full-stack accounting system for Gas Stations, features include inventory management, sales tracking, customer management, Employees Management, Pay Rolls and Admin dashboard Plus Multi station support and many more features.",
-    image: gasImg,
-    type: "web",
-    techStack: ["React js","Tailwind CSS", ".Net Core 8", "MySQL"],
-    liveUrl: "https://gasstationfe.abaalsoftware.com/login",
-    githubUrl: null,
-    playStoreUrl: null,
-    featured: true
-  },
-  // Mobile gas station // https://play.google.com/apps/testing/com.abaalsoftware.gas_station
-  {
-    id: 5,
-    name: "Gas Station Mobile App",
-    description: "A mobile application for gas station management, features include inventory management, sales tracking, customer management, Dipping balance tracking, and Station Overview dashboard Plus Multi station support and many more features.",
-    image: gas_app,
-    type: "mobile",
-    techStack: ["Flutter", "GetX", "Dart"],
-    liveUrl: "",
-    githubUrl: null,
-    playStoreUrl: "https://play.google.com/store/apps/details?id=com.abaalsoftware.gas_station",
-    featured: false
-  }
+  // {
+  //   id: 4,
+  //   name: "Gas Station Management System",
+  //   description: "A full-stack accounting system for Gas Stations, features include inventory management, sales tracking, customer management, Employees Management, Pay Rolls and Admin dashboard Plus Multi station support and many more features.",
+  //   image: gasImg,
+  //   type: "web",
+  //   techStack: ["React js","Tailwind CSS", ".Net Core 8", "MySQL"],
+  //   liveUrl: "https://gasstationfe.abaalsoftware.com/login",
+  //   githubUrl: null,
+  //   playStoreUrl: null,
+  //   featured: true
+  // },
+  // // Mobile gas station // https://play.google.com/apps/testing/com.abaalsoftware.gas_station
+  // {
+  //   id: 5,
+  //   name: "Gas Station Mobile App",
+  //   description: "A mobile application for gas station management, features include inventory management, sales tracking, customer management, Dipping balance tracking, and Station Overview dashboard Plus Multi station support and many more features.",
+  //   image: gas_app,
+  //   type: "mobile",
+  //   techStack: ["Flutter", "GetX", "Dart"],
+  //   liveUrl: "",
+  //   githubUrl: null,
+  //   playStoreUrl: "https://play.google.com/store/apps/details?id=com.abaalsoftware.gas_station",
+  //   featured: false
+  // }
 ]
 
 
